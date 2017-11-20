74bc9293f6d25157fc8620997494e284b3ff49bb
William Brooks <brooks.william@gmail.com>
2017-11-20 09:48:20 -0800

    updated sql schema and seeds

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,9 @@
-INSERT INTO burgers (burger_name) VALUES ('Bison');
-INSERT INTO burgers (burger_name) VALUES ('Elk');
-INSERT INTO burgers (burger_name) VALUES ('Kangaroo', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Avocado', true);
-INSERT INTO burgers (burger_name, devoured) VALUES ('Kale', true);
-INSERT INTO burgers (burger_name) VALUES ('Waygu Beef');
+INSERT INTO `burgers` (`id`, `burger_name`, `devoured`, `date`, `ingredients`)
+VALUES
+	(1, 'Bison', 1, '2017-11-17 18:31:31', 'Lettuce, Tomato'),
+	(2, 'Elk', 1, '2017-11-17 18:31:31', 'Tomato, Mayo'),
+	(3, 'Avocado', 0, '2017-11-17 18:31:31', 'Kale, Beets'),
+	(4, 'Kale', 1, '2017-11-17 18:31:31', 'Avocado, Tomato'),
+	(5, 'Waygu Beef', 0, '2017-11-17 18:31:31', 'Cheese, Tomato'),
+	(20, 'Avocado and Turkey Sandwich', 1, '2017-11-18 14:47:29', 'Lettuce, Cranberry'),
+	(21, 'Bison 2', 1, '2017-11-19 12:45:57', 'Just Bison');
